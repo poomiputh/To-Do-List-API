348c20201ccac34e5591395a679fc532a3df122a
wisel <wisel@80CH>
2024-04-26 00:10:35 +0700

    Add project files.

---
+++ Controllers/TodoEntryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace To_Do_List_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoEntryController : ControllerBase
+    {
+        private static List<TodoEntry> _todoEntries = new List<TodoEntry>();
+        
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("Hello World!");
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] TodoEntry entry)
+        {
+            _todoEntries.Add(entry);
+            return Created($"/{entry.Id}", entry);
+        }
+    }
+}
